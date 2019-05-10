b4b2171d3165d96f008e946820ddaf5aa07753b9
mushood <mushood.badulla@gmail.com>
2019-05-10 07:30:56 +0400

    user db setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('last_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('phone')->nullable();
+
+            $table->text('api_token')->nullable();
+            $table->text('refresh_token')->nullable();
+            $table->datetime('token_expiration')->nullable();
+            $table->string('provider')->nullable();
+            $table->string('provider_id')->nullable();
+
+            $table->softDeletes();
+
             $table->rememberToken();
             $table->timestamps();
         });
