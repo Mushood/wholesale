f1f88ad87cf16d9ddbd78eb345f894602641646f
mushood <mushood.badulla@gmail.com>
2019-05-12 13:55:50 +0400

    languages data import

---
+++ database/seeds/LanguageTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LanguageTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // File obtained from https://gist.github.com/piraveen/fafd0d984b2236e809d03a0e306c8a4d
+        $filePath = storage_path('app/' . \App\Models\Language::LANGUAGE_SEED_FILE);
+
+        if (file_exists($filePath)) {
+            // Read JSON file
+            $json = file_get_contents($filePath);
+
+            //Decode JSON
+            $languages = json_decode($json,true);
+            foreach ($languages as $key => $language) {
+                \App\Models\Language::create([
+                    'code'      => $key,
+                    'name'      => $language['name'],
+                    'native'    => $language['nativeName']
+                ]);
+            }
+        }
+    }
+}
