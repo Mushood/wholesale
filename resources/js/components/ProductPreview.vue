080fb597c7b7f8281eb4e533d00f42cb51380755
mushood <mushood.badulla@gmail.com>
2019-05-29 19:15:40 +0400

    Products landing

---
+++ resources/js/components/ProductPreview.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="product-item">
+        <div class="pi-pic">
+            <div class="tag-sale">ON SALE</div>
+            <img src="/img/product/6.jpg" alt="">
+            <div class="pi-links">
+                <a href="#" class="add-card"><i class="flaticon-bag"></i><span>ADD TO CART</span></a>
+                <a href="#" class="wishlist-btn"><i class="flaticon-heart"></i></a>
+                <a href="/product" class="wishlist-btn"><i class="flaticon-gallery"></i></a>
+            </div>
+        </div>
+        <div class="pi-text">
+            <h6>$35,00</h6>
+            <p>{{ product.name }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        props: {
+            product: {
+                required: true,
+            }
+        }
+    }
+</script>
---
+++ resources/js/components/ProductsLanding.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <div class="col-lg-4 col-sm-6" v-for="product in products">
+            <product-preview :product="product"></product-preview>
+        </div>
+        <div class="text-center w-100 pt-3">
+            <button class="site-btn sb-line sb-dark">LOAD MORE</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        data() {
+            return {
+                products: [
+                    {name: 'name'},{name: 'name'},
+                ]
+            }
+        },
+    }
+</script>
